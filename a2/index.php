95af7b65e878e6bf75a44c0713f0d285ed7e0274
max thum <s4014489@student.rmit.edu.au>
2024-09-29 01:54:44 +1000

    establishing environment.
    this commit was to focus on the establishment of the A2 environment. Removed all HTML references and started writing in PHP for this task.
    Likewise, this was an early build without many of the PHP features been implemented. Next commit will contain updates relating to the PHP and establishment of the SQL server.

---
+++ a2/index.php
@@ -0,0 +1,11 @@
+<?php include ('db_connect.inc'); ?>
+<?php include ('header.inc'); ?>
+<?php include ('nav.inc'); ?>
+
+<main> 
+    <h1>My Account</h1>
+    <p> This is the home page. </p> 
+</main> 
+</body>
+
+<?php include ('footer.inc'); ?>
