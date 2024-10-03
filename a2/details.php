219a508cff773c827131ce69dcef01bb67a59d56
Max Thum <77317317+maxxthum@users.noreply.github.com>
2024-10-04 03:15:17 +1000

    Update details.php

--- a2/details.php
+++ a2/details.php
@@ -1,7 +1,26 @@
 <?php include('includes/db_connect.inc'); ?>
 <?php include('includes/header.inc'); ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<import> 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Permanent+Marker&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+<link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
 
+</import>
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Details - Pets Victoria</title>
+    <meta name="author" content="Max Thum">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
 <?php
 if (isset($_GET['petid'])) {
