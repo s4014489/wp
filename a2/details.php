ee4d10ccc1ed82a46dbb846dfd9fd6fc49964124
Max Thum <77317317+maxxthum@users.noreply.github.com>
2024-10-04 11:34:31 +1000

    details.php fix. functional

--- a2/details.php
+++ a2/details.php
@@ -34,31 +34,38 @@ echo "Pet ID: $petid<br>";
 
   $query = "SELECT * FROM pets WHERE petid = '$petid'";
   $result = mysqli_query($conn, $query);
+
+
   if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
+    $row = $result->fetch_assoc();
   } else {
     echo "Pet not found";
     exit;
   }
-} else {
-  echo "Pet ID not provided";
-  exit;
 }
 ?>
+
 
   <img src="<?php echo 'images/' . $row["image"]; ?>" class="img-fluid">
 <BR> 
 <BR> 
   <h1>Pet Details</h1>
-<p>Pet Name: <?php echo $row["petname"]; ?></p>
+      <li>
+      <p>Age: <?php echo $row["age"]; ?></p>
+      </li>
+    <li>
+      <p>Type: <?php echo $row["type"]; ?></p>
+        </li>
+    <li>
+      <p>Location: <?php echo $row["location"]; ?></p>
+    </li>
+  </ul>
+
+
+<h3>Pet Name: <?php echo $row["petname"]; ?></h3p>
 <p>Description: <?php echo $row["description"]; ?></p>
-<p>Age: <?php echo $row["age"]; ?></p>
-<p>Type: <?php echo $row["type"]; ?></p>
-<p>Location: <?php echo $row["location"]; ?></p>
 
 
-// Close MySQL connection
-mysqli_close($conn);
-?>
+
 <?php include('includes/footer.inc'); ?>
 ?> 
